5a2a9eb887f541e4e0a9f8d131c864e853d4741c
PauloHMoreti <paulohh77@hotmail.com>
2025-12-12 16:34:41 -0300

    feat: add category and meal components, models, and dummy data
    - Implemented CategoryItem and MealItem components for displaying categories and meals.
    - Created Category and Meal models to structure data.
    - Added dummy data for categories and meals to facilitate testing.
    - Developed CategoriesScreen and CategoriesMealsScreen for navigation and displaying meals by category.
    - Introduced AppRoutes for managing named routes in the application.

--- meals/lib/main.dart
+++ meals/lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:meals/screens/categories_screen.dart';
+import 'screens/categories_meals_screen.dart';
+import 'utils/app_routes.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -10,57 +11,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Vamos Cozinhar?',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        primarySwatch: Colors.pink,
+        fontFamily: "Raleway",
+        textTheme: ThemeData.light().textTheme.copyWith(
+          titleMedium: TextStyle(fontSize: 20, fontFamily: "RobotoCondensed"),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      routes: {
+        AppRoutes.home: (_) => CategoriesScreen(),
+        AppRoutes.categoriesMeals: (ctx) => CategoriesMealsScreen(),
+      },
     );
   }
 }
