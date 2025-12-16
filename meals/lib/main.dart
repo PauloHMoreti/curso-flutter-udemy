33f1e4b68fecc3cc6ecbb400ca889e3a81598f5b
PauloHMoreti <paulohh77@hotmail.com>
2025-12-16 10:33:45 -0300

    refactor: remove unused import for categories_screen

--- meals/lib/main.dart
+++ meals/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:meals/screens/categories_screen.dart';
 import 'screens/categories_meals_screen.dart';
 import 'screens/meal_detail_screen.dart';
 import 'screens/tabs_screen.dart';
